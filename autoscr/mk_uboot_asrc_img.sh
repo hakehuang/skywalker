db443d2b64e7f3e15832f7fa53106ea9f0d4dc1f
Hake Huang <b20222@freescale.com>
2010-12-09 19:35:01 +0800

    add autoscr
    Signed-off-by: Hake Huang <b20222@freescale.com>

---
+++ autoscr/mk_uboot_asrc_img.sh
@@ -0,0 +1,23 @@
+bin_prog=./mkimage
+PERFIX=
+PLATFORM="25 31 35 37 51 50 53"
+
+if [ $# -lt 1 ]; then
+  echo "do all platform"
+  for i in $PLATFORM
+  do
+  if [ -e mx${i}_src.txt ]; then
+   $bin_prog -A ARM -O linux -T script -C none -a 0 -e 0 -n "autoscr script" -d mx${i}_src.txt ${PERFIX}mx${i}_asrc_daily.img || return 1
+  else
+   echo " no source file for $i"
+  fi
+  done
+
+else
+  echo "do $1 only" 
+  if [ -e mx${1}_src.txt ]; then
+   $bin_prog -A ARM -O linux -T script -C none -a 0 -e 0 -n "autoscr script" -d mx${1}_src.txt ${PERFIX}mx${1}_asrc_daily.img || return 1
+  else
+   echo " no source file for $1"
+  fi
+fi 
