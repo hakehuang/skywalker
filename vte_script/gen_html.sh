38a766eae4f3d9cceb060922fd6033b10d35112c
Hake Huang <b20222@freescale.com>
2010-12-22 21:16:25 +0800

    add _d
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- vte_script/gen_html.sh
+++ vte_script/gen_html.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -x
 
 BASE=/rootfs/wb
-PRJ="vte_mx50"
+PRJ="vte_mx50 vte_mx50_d"
 
 for i in $PRJ
 do
 LTPROOT=$BASE/$i
+if [ -e $LTPROOT/output/latest_test_report ] ; then
 . $LTPROOT/output/latest_test_report
 OUTPUT_DIRECTORY=$(basename $OUTPUT_DIRECTORY)
 export OUTPUT_DIRECTORY=${LTPROOT}/output/$OUTPUT_DIRECTORY
@@ -14,4 +15,5 @@ export TEST_OUTPUT_DIRECTORY="${LTPROOT}/output"
 export TEST_LOGS_DIRECTORY=${LTPROOT}/$TEST_LOGS_DIRECTORY
 export HTMLFILE=${LTPROOT}/output/$HTMLFILE
 /usr/bin/perl $LTPROOT/bin/genhtml.pl $LTPROOT/tools/html_report_header.txt test_start test_end test_output execution_status $OUTPUT_DIRECTORY  > $HTMLFILE
+fi
 done
