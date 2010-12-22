edda0627d729592bda8bdd17c2cca5a471291a23
Hake Huang <b20222@freescale.com>
2010-12-22 21:06:35 +0800

    add get html script for php usage
    Signed-off-by: Hake Huang <b20222@freescale.com>

---
+++ vte_script/gen_html.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+BASE=/rootfs/wb
+PRJ="vte_mx50"
+
+for i in $PRJ
+do
+LTPROOT=$BASE/$i
+. $LTPROOT/output/latest_test_report
+OUTPUT_DIRECTORY=$(basename $OUTPUT_DIRECTORY)
+export OUTPUT_DIRECTORY=${LTPROOT}/output/$OUTPUT_DIRECTORY
+export LOGS_DIRECTORY="${LTPROOT}/results"
+export TEST_OUTPUT_DIRECTORY="${LTPROOT}/output"
+export TEST_LOGS_DIRECTORY=${LTPROOT}/$TEST_LOGS_DIRECTORY
+export HTMLFILE=${LTPROOT}/output/$HTMLFILE
+/usr/bin/perl $LTPROOT/bin/genhtml.pl $LTPROOT/tools/html_report_header.txt test_start test_end test_output execution_status $OUTPUT_DIRECTORY  > $HTMLFILE
+done
---
+++ vte_script/gen_report.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<?php
+system("/rootfs/wb/gen_html.sh");
+echo "result generated!"
+?>
+
+</body>
+</html>
