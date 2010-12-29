6a61659c91d7dcd786428da012de34fa1d0af3e7
Hake Huang <b20222@freescale.com>
2010-12-29 11:40:41 +0800

    upgrade the gen html method
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- vte_script/gen_html.sh
+++ vte_script/gen_html.sh
@@ -1,12 +1,21 @@
-#!/bin/sh -x
+#!/bin/sh
 
 BASE=/rootfs/wb
-PRJ="vte_mx50 vte_mx50_d"
+TARGET_OUTPUT_BASE=${BASE}/daily_reports/skywalker
+
+PRJ="vte_mx50"
 MAX_CASES=50
+
+#determinate the year and week
+YEAR=$(date +%Y)
+WEEK=$(date +%U)
+DAY=$(date +%d)
 
 for i in $PRJ
 do
 	LTPROOT=$BASE/$i
+  OUT_BASE=${TARGET_OUTPUT_BASE}/${i}/${YEAR}/WW${WEEK}/${DAY}/
+	mkdir -p $OUT_BASE
 	if [ -e $LTPROOT/output/latest_test_report ] ; then
 	. $LTPROOT/output/latest_test_report
 	OUTPUT_DIRECTORY=$(basename $OUTPUT_DIRECTORY)
@@ -14,34 +23,40 @@ do
 	export LOGS_DIRECTORY="${LTPROOT}/results"
 	export TEST_OUTPUT_DIRECTORY="${LTPROOT}/output"
 	export TEST_LOGS_DIRECTORY=${LTPROOT}/$TEST_LOGS_DIRECTORY
-	export HTMLFILE=${LTPROOT}/output/$HTMLFILE
+	export HTMLFILE=${OUT_BASE}$HTMLFILE
 	#judge whether the log file is too large, if so split them
 	case_cnt=$(cat $OUTPUT_DIRECTORY | grep -i "<<<test_start>>>" | wc -l)
 	if [ $case_cnt -gt $MAX_CASES ]; then
 	#log files seems too large splits them 
-		case_cnt=0
+		icnt=0
 		file_cnt=0
-		touch ${OUTPUT_DIRECTORY}_0
+		touch ${OUT_BASE}/$(basename ${OUTPUT_DIRECTORY})_0
+		echo "" > ${OUT_BASE}/$(basename ${OUTPUT_DIRECTORY})_0
 		cat $OUTPUT_DIRECTORY | while read LINE
 		do
-			echo $LINE >> ${OUTPUT_DIRECTORY}_${file_cnt}
+			echo $LINE >> ${OUT_BASE}/$(basename ${OUTPUT_DIRECTORY})_${file_cnt}
       taged=$(echo $LINE | grep -i "<<<test_end>>>"| wc -l)
 			if [ $taged -eq 1 ]; then
-			  icnt=$(expr $case_cnt + 1)
+			  icnt=$(expr $icnt + 1)
 				if [ $icnt -eq $MAX_CASES ]; then
+				 oHTMLFILE=$(basename $HTMLFILE .html)_${file_cnt}.html
+				 echo "output to ${OUT_BASE}/$oHTMLFILE"
+				 /usr/bin/perl $LTPROOT/bin/genhtml.pl $LTPROOT/tools/html_report_header.txt \
+				 test_start test_end test_output execution_status ${OUT_BASE}/$(basename ${OUTPUT_DIRECTORY})_${file_cnt}  > ${OUT_BASE}/${oHTMLFILE}
          file_cnt=$(expr $file_cnt + 1)
 				 icnt=0
-				 oHTMLFILE=$(basename $HTMLFILE .html)_${file_cnt}.html
-				 /usr/bin/perl $LTPROOT/bin/genhtml.pl $LTPROOT/tools/html_report_header.txt \
-				 test_start test_end test_output execution_status ${OUTPUT_DIRECTORY}_${file_cnt}  > $oHTMLFILE
-				 touch ${OUTPUT_DIRECTORY}_${file_cnt}
+				 touch ${OUT_BASE}/$(basename ${OUTPUT_DIRECTORY})_${file_cnt}
+				 echo "" > ${OUT_BASE}/$(basename ${OUTPUT_DIRECTORY})_${file_cnt}
 				fi
 			fi
 		done
+		file_cnt=$(echo "$case_cnt/$MAX_CASES" | bc)
+		oHTMLFILE=$(basename $HTMLFILE .html)_${file_cnt}.html
+		echo "output to ${OUT_BASE}/$oHTMLFILE"
 		/usr/bin/perl $LTPROOT/bin/genhtml.pl $LTPROOT/tools/html_report_header.txt \
-		test_start test_end test_output execution_status ${OUTPUT_DIRECTORY}_${file_cnt}  > $oHTMLFILE
-	else
-	/usr/bin/perl $LTPROOT/bin/genhtml.pl $LTPROOT/tools/html_report_header.txt test_start test_end test_output execution_status $OUTPUT_DIRECTORY  > $HTMLFILE
+		test_start test_end test_output execution_status ${OUT_BASE}/$(basename ${OUTPUT_DIRECTORY})_${file_cnt} > ${OUT_BASE}/${oHTMLFILE}
+		else
+		/usr/bin/perl $LTPROOT/bin/genhtml.pl $LTPROOT/tools/html_report_header.txt test_start test_end test_output execution_status $OUTPUT_DIRECTORY  > $HTMLFILE
+		fi
 	fi
-fi
 done
--- vte_script/gen_report.php
+++ vte_script/gen_report.php
@@ -1,9 +1,9 @@
 <html>
 <body>
-
+<marquee direction=left scrollamount=6>When I move, generating OK</marquee>
 <?php
 system("/rootfs/wb/gen_html.sh");
-echo "result generated!"
+echo "<p>result generated!</p>";
 ?>
 
 </body>
