62135412f9f1329374fe093976902a11a1139693
Hake Huang <b20222@freescale.com>
2011-08-26 12:49:55 +0800

    linaro nano does not support nfs by default
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- vte_script/linaro_daily.sh
+++ vte_script/linaro_daily.sh
@@ -19,14 +19,14 @@ wget ${WBASE}/${NANOBASE}/${NANO} || exit 2
 
 sudo cat << EOF > fstab
 proc            /proc           proc    nodev,noexec,nosuid 0       0
-10.192.224.218:/rootfs/imx53_rootfs_nano   /   nfs  rw,noatime,nolock,vers=3 1 1
+#10.192.224.218:/rootfs/imx53_rootfs_nano   /   nfs  rw,noatime,nolock,vers=3 1 1
 none       /tmp   tmpfs defaults 0 0
 none       /var/run tmpfs defaults 0 0
 none       /var/lock tmpfs defaults 0 0
 none       /var/tmp  tmpfs defaults 0 0
-10.192.225.222:/rootfs/wb /mnt/nfs nfs rw,noatime,nolock,vers=3 1 1
-10.192.224.218:/rootfs /mnt/nfs_root nfs rw,noatime,nolock,vers=3 1 1
-10.192.225.226:/01_CodecVectors /mnt/stream nfs rw,noatime,nolock,vers=3 1 1
+#10.192.225.222:/rootfs/wb /mnt/nfs nfs rw,noatime,nolock,vers=3 1 1
+#10.192.224.218:/rootfs /mnt/nfs_root nfs rw,noatime,nolock,vers=3 1 1
+#10.192.225.226:/01_CodecVectors /mnt/stream nfs rw,noatime,nolock,vers=3 1 1
 EOF
 
 #untar the rootfs
@@ -35,6 +35,10 @@ sudo mkdir $ROOTFS
 sudo tar xzvf ${NANO} -C $ROOTFS || exit 3
 
 sudo cp -f fstab $ROOTFS/binary/etc/
+
+sudo mkdir $ROOTFS/binary/etc/mnt/nfs
+sudo mkdir $ROOTFS/binary/etc/mnt/nfs_root
+sudo mkdir $ROOTFS/binary/etc/mnt/stream
 
 #untart the hwpack
 rm -rf hwpack
