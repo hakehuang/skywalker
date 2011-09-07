8f91b7656f4291c44ada0ea53bbe202068b47609
Hake Huang <b20222@freescale.com>
2011-09-07 18:16:13 +0800

    path kernel include to libs
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- vte_script/dbuild.sh
+++ vte_script/dbuild.sh
@@ -94,7 +94,7 @@ make_libs()
   git checkout build || git add . && git commit -s -m"build $(date +%m%d)" && git checkout build
   git checkout -b build_${2} build
   make distclean
-  make PLATFORM=${2} CROSS_COMPILE=arm-none-linux-gnueabi-  || return 1
+  make PLATFORM=${2} CROSS_COMPILE=arm-none-linux-gnueabi- INCLUDE="-I${KERNEL_DIR}/include -I${KERNEL_DIR}/drivers/mxc/security/rng/include -I${KERNEL_DIR}/drivers/mxc/security/sahara2/include" || return 1
   sudo make DEST_DIR=${TARGET_ROOTFS}/imx${3}_rootfs install || return 2 
 }
 
