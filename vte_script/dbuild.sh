401a155acfd26d9c54fee9fb494040cf1f5726b8
Hake Huang <b20222@freescale.com>
2012-03-26 10:27:16 +0800

    upgrade to new tool chain
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- vte_script/dbuild.sh
+++ vte_script/dbuild.sh
@@ -12,7 +12,7 @@ UBOOT_BRH=imx_v2009.08
 VTE_TARGET_PRE=/mnt/vte/
 VTE_TARGET_PRE2=/mnt/nfs/
 TARGET_ROOTFS=/mnt/nfs_root/
-ROOTDIR=/home/ltib2/daily_build/
+ROOTDIR=/home/ltib2/daily_build/tool_chain/
 KERNEL_DIR=${ROOTDIR}/linux-2.6-imx/
 UBOOT_DIR=${ROOTDIR}/uboot-imx
 VTE_DIR=${ROOTDIR}/vte
@@ -25,7 +25,8 @@ LIB_DIR=${ROOTDIR}/linux-lib
 
 all_one_branch=n
 
-export PATH=$PATH:/opt/freescale/usr/local/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin
+TOOL_CHAIN=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/
+export PATH=$PATH:/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/
 
 RC=0
 
@@ -38,6 +39,7 @@ declare -a u_boot_configs;
 declare -a kernel_configs;
 declare -a vte_configs;
 declare -a unit_test_configs;
+declare -a rootfs_apd;
 # As bash only support 1 dimension array below sequence is our assumption
 # 0   1  2  3  4  5  6  7  8  9  10 11
 #(23 25 28 31 35 37 50 50  51 53 53 61)
@@ -74,6 +76,8 @@ unit_test_configs=("IMX233" "IMX25" "IMX28" "IMX3" "IMX3" "IMX3" "IMX5" \
 linux_libs_platfm=("NULL" "NULL" "NULL" "NULL" "NULL" "IMX37_3STACK" "NULL" "NULL" "IMX51" "IMX53" "IMX53" "IMX6Q" "IMX6Q" "IMX6Q" "IMX6Q" "IMX6Q");
 linux_libs_branch=("master" "master" "master" "master" "master" "master" "master" \
 "master" "master" "master" "master" "master" "master" "master" "master" "master");
+#rootfs and vte apendix
+rootfs_apd=("" "" "" "" "" "" "" "" "" "" "" "" "" "" "_s" "_s");
 
 branch_libs()
 {
@@ -83,7 +87,7 @@ branch_libs()
   fi
   cd $LIB_DIR
   git checkout master
-  git pull  
+  git pull 
 }
 
 make_libs()
@@ -101,8 +105,8 @@ make_libs()
   git checkout build || git add . && git commit -s -m"build $(date +%m%d)" && git checkout build
   git checkout -b build_${2} build
   make clean
-  make PLATFORM=${2} CROSS_COMPILE=arm-none-linux-gnueabi- INCLUDE="-I${KERNEL_DIR}/include -I${KERNEL_DIR}/drivers/mxc/security/rng/include -I${KERNEL_DIR}/drivers/mxc/security/sahara2/include" -k || iRC=1
-  sudo make DEST_DIR=${TARGET_ROOTFS}/imx${3}_rootfs install -k || iRC=$(expr $iRC + 1)
+  make PLATFORM=${2} CROSS_COMPILE=${${TOOL_CHAIN}}arm-none-linux-gnueabi- INCLUDE="-I${KERNEL_DIR}/include -I${KERNEL_DIR}/drivers/mxc/security/rng/include -I${KERNEL_DIR}/drivers/mxc/security/sahara2/include" -k || iRC=1
+  sudo make DEST_DIR=${TARGET_ROOTFS}/imx${3}_rootfs${4} install -k || iRC=$(expr $iRC + 1)
   return $iRC
 }
 
@@ -116,8 +120,8 @@ deploy_firmware()
   cd $FIRMWARE_DIR
   git checkout master && git pull
   if [ -e ${FIRMWARE_DIR}/firmware ]; then
-    sudo rm -rf ${TARGET_ROOTFS}/imx${1}_rootfs/lib/firmware
-    sudo cp -af ${FIRMWARE_DIR}/firmware ${TARGET_ROOTFS}/imx${1}_rootfs/lib/ || return 1
+    sudo rm -rf ${TARGET_ROOTFS}/imx${1}_rootfs${2}/lib/firmware
+    sudo cp -af ${FIRMWARE_DIR}/firmware ${TARGET_ROOTFS}/imx${1}_rootfs${2}/lib/ || return 1
   fi 
   return 0
 }
@@ -142,27 +146,27 @@ make_unit_test()
     echo "VTE daily build found unit test change" | mutt -s "the unit test count is $ucs changed" \
 		b20222@freescale.com 
  fi
- sudo cp unit_test ${VTE_TARGET_PRE}/vte_mx${2}_d/runtest/
- sudo cp unit_test ${VTE_TARGET_PRE2}/vte_mx${2}_d/runtest/
+ sudo cp unit_test ${VTE_TARGET_PRE}/vte_mx${2}_${3}d/runtest/
+ sudo cp unit_test ${VTE_TARGET_PRE2}/vte_mx${2}_${3}d/runtest/
  old_ut_plat=$2
  old_ut_rc=0
  PLATFORM=$1
  KERNELDIR=$KERNEL_DIR
  KBUILD_OUTPUT=$KERNEL_DIR
- INCLUDE="-I${TARGET_ROOTFS}/imx${2}_rootfs/usr/include \
- -I${TARGET_ROOTFS}/imx${2}_rootfs/usr/src/linux/include \
+ INCLUDE="-I${TARGET_ROOTFS}/imx${2}_rootfs${3}/usr/include \
+ -I${TARGET_ROOTFS}/imx${2}_rootfs${3}/usr/src/linux/include \
  -I./include/"
- LIB="-L/mnt/nfs_root/imx${2}_rootfs/usr/lib"
+ LIB="-L/mnt/nfs_root/imx${2}_rootfs${3}/usr/lib"
  make distclean
- make -C module_test KBUILD_OUTPUT=$KBUILD_OUTPUT LINUXPATH=$KERNELDIR  CC=arm-none-linux-gnueabi-gcc \
- CROSS_COMPILE=arm-none-linux-gnueabi- || old_ut_rc=1
- make -j1 PLATFORM=$PLATFORM INC="${INCLUDE}" LIBS=${LIB} test  CC=arm-none-linux-gnueabi-gcc \
- CROSS_COMPILE=arm-none-linux-gnueabi- || old_ut_rc=$(expr $old_ut_rc + 2)
+ make -C module_test KBUILD_OUTPUT=$KBUILD_OUTPUT LINUXPATH=$KERNELDIR  CC=${TOOL_CHAIN}arm-none-linux-gnueabi-gcc \
+ CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- || old_ut_rc=1
+ make -j1 PLATFORM=$PLATFORM INC="${INCLUDE}" LIBS=${LIB} test  CC=${TOOL_CHAIN}arm-none-linux-gnueabi-gcc \
+ CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- || old_ut_rc=$(expr $old_ut_rc + 2)
  sudo make -C module_test -j1 LINUXPATH=$KERNELDIR KBUILD_OUTPUT=$KBUILD_OUTPUT \
- CROSS_COMPILE=arm-none-linux-gnueabi- \
- DEPMOD=/bin/true INSTALL_MOD_PATH=${TARGET_ROOTFS}/imx${2}_rootfs install -k || old_ut_rc=$(expr $old_ut_rc + 4)
- sudo  make PLATFORM=$PLATFORM DESTDIR=${TARGET_ROOTFS}/imx${2}_rootfs/unit_tests \
- CROSS_COMPILE=arm-none-linux-gnueabi- install || old_ut_rc=$(expr $old_ut_rc + 8)
+ CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- \
+ DEPMOD=/bin/true INSTALL_MOD_PATH=${TARGET_ROOTFS}/imx${2}_rootfs${3} install -k || old_ut_rc=$(expr $old_ut_rc + 4)
+ sudo  make PLATFORM=$PLATFORM DESTDIR=${TARGET_ROOTFS}/imx${2}_rootfs${3}/unit_tests \
+ CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- install || old_ut_rc=$(expr $old_ut_rc + 8)
  return $old_ut_rc
 }
 
@@ -175,7 +179,7 @@ make clean
 make  CC=gcc PLATFORM=MX$3
 $UCONFDIR/u-config -s ${1}-config.txt u-boot-${1}-config.bin
 #rm -f ${1}_config.txt
-sudo cp u-boot-${1}-config.bin /mnt/nfs_root/imx${3}_rootfs/root/u-boot-${1}-config.bin || return 3
+sudo cp u-boot-${1}-config.bin /mnt/nfs_root/imx${3}_rootfs${4}/root/u-boot-${1}-config.bin || return 3
 rm -rf  u-boot-${1}-config.bin
 }
 
@@ -183,14 +187,14 @@ make_uboot()
 {
 echo "make uboot $2 with $1"
 cd $UBOOT_DIR
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- distclean
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- $1 || return 1
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- || return 2
+make ARCH=arm CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- distclean
+make ARCH=arm CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- $1 || return 1
+make ARCH=arm CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- || return 2
 scp u-boot.bin root@10.192.225.218:/tftpboot/u-boot-${3}_d.bin
 scp u-boot.bin root@10.192.225.218:/var/ftp/u-boot-${3}_d.bin
 scp u-boot.bin ubuntu@10.192.244.7:/var/lib/tftpboot/u-boot-${3}_d.bin
-sudo cp u-boot.bin /mnt/nfs_root/imx${2}_rootfs/root/u-boot-${3}_d.bin || return 3
-make_uboot_config $3 $(git log | head -1 | cut -d " " -f 2 | cut -c 1-6) $2 || return 3 
+sudo cp u-boot.bin /mnt/nfs_root/imx${2}_rootfs${4}/root/u-boot-${3}_d.bin || return 3
+make_uboot_config $3 $(git log | head -1 | cut -d " " -f 2 | cut -c 1-6) $2 $4 || return 3 
 return 0
 }
 
@@ -201,10 +205,10 @@ cd $KERNEL_DIR
 if [ "$old_kernel_config" = $1 ];then
 if [ "$old_kernel_rc" -eq 0 ]; then
 #sudo rm -rf ${TARGET_ROOTFS}/imx${2}_rootfs/lib/modules/*-daily*
-sudo make ARCH=arm modules_install INSTALL_MOD_PATH=${TARGET_ROOTFS}/imx${2}_rootfs || return 3
-scp arch/arm/boot/uImage root@10.192.225.218:/tftpboot/uImage_mx${2}_d
-scp arch/arm/boot/uImage root@10.192.225.218:/var/ftp/uImage_mx${2}_d
-scp arch/arm/boot/uImage ubuntu@10.192.244.7:/var/lib/tftpboot/uImage_mx${2}_d
+sudo make ARCH=arm modules_install INSTALL_MOD_PATH=${TARGET_ROOTFS}/imx${2}_rootfs${3} || return 3
+scp arch/arm/boot/uImage root@10.192.225.218:/tftpboot/uImage_mx${2}_${3}d
+scp arch/arm/boot/uImage root@10.192.225.218:/var/ftp/uImage_mx${2}_${3}d
+scp arch/arm/boot/uImage ubuntu@10.192.244.7:/var/lib/tftpboot/uImage_mx${2}_${3}d
 fi
 return $old_kernel_rc
 fi
@@ -214,15 +218,15 @@ git checkout build || git add . && git commit -s -m"build $(date +%m%d)" && git 
 git checkout -b build_${2} build
 make distclean
 echo "-daily"  > localversion
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- $1 || return 1
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- -j 2 uImage|| return 2
+make ARCH=arm CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- $1 || return 1
+make ARCH=arm CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- -j 2 uImage|| return 2
 KERNEL_VER=$(./scripts/setlocalversion)
-sudo rm -rf ${TARGET_ROOTFS}/imx${2}_rootfs/lib/modules/*-daily*
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- -j 2 modules|| return 4
-sudo make ARCH=arm modules_install INSTALL_MOD_PATH=${TARGET_ROOTFS}/imx${2}_rootfs || return 3
-sudo make ARCH=arm headers_install INSTALL_HDR_PATH=${TARGET_ROOTFS}/imx${2}_rootfs/usr/src/linux/ || return 5
-scp arch/arm/boot/uImage root@10.192.225.218:/tftpboot/uImage_mx${2}_d
-scp arch/arm/boot/uImage root@10.192.225.218:/var/ftp/uImage_mx${2}_d
+sudo rm -rf ${TARGET_ROOTFS}/imx${2}_rootfs${3}/lib/modules/*-daily*
+make ARCH=arm CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi- -j 2 modules|| return 4
+sudo make ARCH=arm modules_install INSTALL_MOD_PATH=${TARGET_ROOTFS}/imx${2}_rootfs${3} || return 3
+sudo make ARCH=arm headers_install INSTALL_HDR_PATH=${TARGET_ROOTFS}/imx${2}_rootfs${3}/usr/src/linux/ || return 5
+scp arch/arm/boot/uImage root@10.192.225.218:/tftpboot/uImage_mx${2}_${3}d
+scp arch/arm/boot/uImage root@10.192.225.218:/var/ftp/uImage_mx${2}_${3}d
 old_kernel_rc=0
 return 0
 }
@@ -232,13 +236,13 @@ make_vte()
 ret=0
 cd $VTE_DIR
 if [ "$old_vte_config" = $1 ]; then
- if [ "$old_vte_rc" -eq 0 ] && [ -e $VTE_DIR/install ]; then
-   sudo cp -a install/* ${VTE_TARGET_PRE}/vte_mx${2}_d/
-   sudo cp -a testcases/bin/* ${VTE_TARGET_PRE}/vte_mx${2}_d/testcases/bin/
-   sudo cp mytest ${VTE_TARGET_PRE}/vte_mx${2}_d/
-   sudo cp -a install/* ${VTE_TARGET_PRE2}/vte_mx${2}_d/
-   sudo cp -a testcases/bin/* ${VTE_TARGET_PRE2}/vte_mx${2}_d/testcases/bin/
-   sudo cp mytest ${VTE_TARGET_PRE2}/vte_mx${2}_d/
+ if [ $old_vte_rc -eq 0 ] && [ -e $VTE_DIR/install ]; then
+   sudo cp -a install/* ${VTE_TARGET_PRE}/vte_mx${2}_${3}d/
+   sudo cp -a testcases/bin/* ${VTE_TARGET_PRE}/vte_mx${2}_${3}d/testcases/bin/
+   sudo cp mytest ${VTE_TARGET_PRE}/vte_mx${2}_${3}d/
+   sudo cp -a install/* ${VTE_TARGET_PRE2}/vte_mx${2}_${3}d/
+   sudo cp -a testcases/bin/* ${VTE_TARGET_PRE2}/vte_mx${2}_${3}d/testcases/bin/
+   sudo cp mytest ${VTE_TARGET_PRE2}/vte_mx${2}_${3}d/
  fi
 return $old_vte_rc
 fi
@@ -246,10 +250,10 @@ old_vte_config=$1
 make distclean
 sudo rm -rf install
 source $1
-export KLINUX_SRCDIR=${TARGET_ROOTFS}/imx${2}_rootfs/usr/src/linux/
+export KLINUX_SRCDIR=${TARGET_ROOTFS}/imx${2}_rootfs${3}/usr/src/linux/
 export KERNEL_SRCDIR=${KERNEL_DIR}
 export KLINUX_BLTDIR=${KERNEL_DIR}
-export CROSS_COMPILE=arm-none-linux-gnueabi-
+export CROSS_COMPILE=${TOOL_CHAIN}arm-none-linux-gnueabi-
 export CC=${CROSS_COMPILE}gcc
 autoreconf -f -i -Wall,no-obsolete
 aclocal -I m4
@@ -258,19 +262,19 @@ automake -a
 make autotools
 ./configure --host=arm-none-linux-gnueabi --prefix=$(pwd)/install CC=${CROSS_COMPILE}gcc
 make
-make vte || exit 1
-make apps || ret=2
+make vte || ret=1
+make apps || exit 1
 make install
 #make ltp tests
 #if [ $BUILD = "y" ]; then
 #sudo scp -r bin/* b17931@survivor:/rootfs/wb/vte_mx${2}_d/bin
 #fi
-sudo cp -a install/* ${VTE_TARGET_PRE}/vte_mx${2}_d/
-sudo cp -a testcases/bin/* ${VTE_TARGET_PRE}/vte_mx${2}_d/testcases/bin/
-sudo cp mytest ${VTE_TARGET_PRE}/vte_mx${2}_d/
-sudo cp -a install/* ${VTE_TARGET_PRE2}/vte_mx${2}_d/
-sudo cp -a testcases/bin/* ${VTE_TARGET_PRE2}/vte_mx${2}_d/testcases/bin/
-sudo cp mytest ${VTE_TARGET_PRE2}/vte_mx${2}_d/
+sudo cp -a install/* ${VTE_TARGET_PRE}/vte_mx${2}_${3}d/
+sudo cp -a testcases/bin/* ${VTE_TARGET_PRE}/vte_mx${2}_${3}d/testcases/bin/
+sudo cp mytest ${VTE_TARGET_PRE}/vte_mx${2}_${3}d/
+sudo cp -a install/* ${VTE_TARGET_PRE2}/vte_mx${2}_${3}d/
+sudo cp -a testcases/bin/* ${VTE_TARGET_PRE2}/vte_mx${2}_${3}d/testcases/bin/
+sudo cp mytest ${VTE_TARGET_PRE2}/vte_mx${2}_${3}d/
 #sudo scp -r testcases/bin/* b17931@survivor:/rootfs/wb/vte_mx${2}_d/testcases/bin
 old_vte_rc=0
 return $ret
@@ -287,21 +291,21 @@ make_tools()
 update_rootfs()
 {	
  cd $SCRPTSDIR
- sudo cp vte ${TARGET_ROOTFS}/imx${1}_rootfs/etc/rc.d/init.d/vte 	
- deploy_firmware $1
+ sudo cp vte ${TARGET_ROOTFS}/imx${1}_rootfs${2}/etc/rc.d/init.d/vte 	
+ deploy_firmware $1 $2
 }
 
 make_target_tools()
 {
  cd $TOOLSDIR
  make clean
- CROSS_COMPILER=arm-none-linux-gnueabi- make || return 10
+ CROSS_COMPILER=${TOOL_CHAIN}arm-none-linux-gnueabi- make || return 10
  sudo cp uclient ${VTE_TARGET_PRE}/tools/
  sudo cp uclient ${VTE_TARGET_PRE2}/tools/
  make clean
  cd $UCONFDIR
  make clean
- make CC=arm-none-linux-gnueabi-gcc PLATFORM=$1 || return 11
+ make CC=${TOOL_CHAIN}arm-none-linux-gnueabi-gcc PLATFORM=$1 || return 11
  sudo cp u-config ${VTE_TARGET_PRE}/tools/
  sudo cp printenv ${VTE_TARGET_PRE}/tools/
  sudo cp setenv ${VTE_TARGET_PRE}/tools/
@@ -316,7 +320,7 @@ sync_server()
  cd $TOOLSDIR
  make clean
  make CC=gcc || return 10
- $TOOLSDIR/uclient 10.192.225.222 12500 ${1}_${2} 
+ #$TOOLSDIR/uclient 10.192.225.222 12500 ${1}_${2} 
  $TOOLSDIR/uclient 10.192.244.61 12500 ${1}_${2} 
 }
 
@@ -454,27 +458,26 @@ branch_libs
 for i in $PLATFORM;
 do
   j=0
-	sync_server $i NOREADY
+  sync_server ${i} NOREADY
   while [ $j -lt $SOC_CNT ];do
    c_plat=${plat_name[${j}]}
    if [ "$c_plat" = $i ];then
      c_soc=${soc_name[${j}]}
      make_target_tools MX${c_soc} 
-     make_uboot ${u_boot_configs[${j}]} $c_soc $c_plat || RC=$(echo $RC uboot_$i)
-     exit 0
+     make_uboot ${u_boot_configs[${j}]} $c_soc $c_plat ${rootfs_apd[${j}]} || RC=$(echo $RC uboot_$i)
      branch_kernel ${kernel_branch[$j]}
-     make_kernel ${kernel_configs[${j}]} $c_soc || old_kernel_rc=$?
+     make_kernel ${kernel_configs[${j}]} $c_soc ${rootfs_apd[${j}]} || old_kernel_rc=$?
      if [ $old_kernel_rc -ne 0 ]; then 
-				RC=$(echo $RC $i)
+	RC=$(echo $RC $i)
      fi
-	 branch_vte ${vte_branch[$j]}
-     make_vte  ${vte_configs[${j}]} $c_soc || old_vte_rc=$?
+     branch_vte ${vte_branch[$j]}
+     make_vte  ${vte_configs[${j}]} $c_soc ${rootfs_apd[${j}]} || old_vte_rc=$?
      if [ $old_vte_rc -ne 0 ]; then
      	RC=$(echo $RC vte_$i)
      fi
-     update_rootfs $c_soc
-     make_libs ${linux_libs_branch[${j}]} ${linux_libs_platfm[${j}]} $c_soc
-     make_unit_test ${unit_test_configs[${j}]} $c_soc || RC=$(echo $RC unit_test_$i) 
+     update_rootfs $c_soc ${rootfs_apd[${j}]}
+     make_libs ${linux_libs_branch[${j}]} ${linux_libs_platfm[${j}]} $c_soc ${rootfs_apd[${j}]}
+     make_unit_test ${unit_test_configs[${j}]} $c_soc ${rootfs_apd[${j}]} || RC=$(echo $RC unit_test_$i) 
      #if [ $old_kernel_rc -eq 0 ] && [ $old_vte_rc -eq 0 ] && [ $(echo $RC | grep uboot_$i | wc -l) -eq 0 ]
      #then
      	sync_server $i READY_KVER${KERNEL_VER}
@@ -487,11 +490,11 @@ done
 
 echo $RC
 if [ "$RC" = "0" ]; then
-echo "VTE daily build with $RC" | mutt -s "VTE daily build OK" \
+echo "VTE new toolchian daily build with $RC" | mutt -s "VTE new tool chain daily build OK" \
 b20222@freescale.com 
 echo build success!!
 else
-echo "VTE daily build with $RC" | mutt -s "VTE daily build Fail" \
+echo "VTE new tool chain daily build with $RC" | mutt -s "VTE new tool chain daily build Fail" \
 b20222@freescale.com
 echo build Fail $RC!
 fi
