4112180180a7c4d2206bb38c17adf5ecac991913
Hake Huang <b20222@freescale.com>
2012-08-20 20:58:29 +0800

    add test result to xiaotian cycle
    Signed-off-by: Hake Huang <b20222@freescale.com>

---
+++ vte_script/client_skywalker_cycle.php
@@ -0,0 +1,36 @@
+<?php
+
+//PrintDebug($argv);
+$params = array('app'=>'xiaotian', 
+                'mod'=>'skywalker',
+                'action'=>'createCycle',
+                'testplan'=>$_SERVER["argv"][1],
+                'txt'=>$_SERVER["argv"][2],
+                'html'=>$_SERVER["argv"][3]
+                );
+//print_r($params);        
+
+try{
+    $soapclient = new SoapClient(null,array(
+        'location'=>"http://umbrella/XiaoTian/NewCode/services/server.php",
+        'uri'=>'server.php',
+        'trace'=>1,
+        'exception'=>0));
+//print_r($soapclient);        
+    $result = $soapclient->startResponse($params);
+    print_r($result); // return the cycle id
+    if (empty($result)){ // fail
+        print_r("Fail to create the cycle.");
+    }
+    else{
+        print_r("Success to create the cycle, cycle_id=$result");
+    }
+}catch(SoapFault $e){
+//print_r($e);
+    echo $e->getMessage();
+}catch(Exception $e){
+//print_r($e);
+    echo $e->getMessage();
+}
+
+?>
